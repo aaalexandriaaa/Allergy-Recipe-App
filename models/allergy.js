fe2430acb4cbaceb4a120f0f1ae890fe2f8a4b0c
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-08-29 16:36:43 -0400

    creating allergy model

---
+++ models/allergy.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+var allergySchema = Schema({
+    gluten: {
+        Type: Boolean,
+        default: true
+    },
+    milk: {
+        Type: Boolean,
+        default: true
+    },
+    eggs: {
+        Type: Boolean,
+        default: true
+    },
+    finFish: {
+        Type: Boolean,
+        default: true
+    },
+    shellfish: {
+        Type: Boolean,
+        default: true
+    },
+    treeNuts: {
+        Type: Boolean,
+        default: true
+    },
+    peanuts: {
+        Type: Boolean,
+        default: true
+    },
+    soybeans: {
+        Type: Boolean,
+        default: true
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Allergy', allergySchema);
