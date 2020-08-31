0d472126967c1f295afb0b9b8766629342f23952
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-08-30 20:51:11 -0400

    setting up allergy router and editing server.js

--- routes/allergies.js
+++ routes/allergies.js
@@ -1,8 +1,9 @@
 var router = require('express').Router();
-var usersCtrl = require('../controllers/allergies');
+var allergiesCtrl = require('../controllers/allergies');
 
 // GET /users
-router.post('/users/:id/allergies', isLoggedIn, usersCtrl.addAllergy);
+router.get('/users/:id/allergies', isLoggedIn, allergiesCtrl.index)
+router.post('/allergies/new', isLoggedIn, allergiesCtrl.create);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
