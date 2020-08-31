bdecdd82abbd8c91cc3b35f995cfa9bbb1e2d041
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-08-31 13:29:09 -0400

    updating route to bring user information into allergy controller fn

--- routes/allergies.js
+++ routes/allergies.js
@@ -3,7 +3,7 @@ var allergiesCtrl = require('../controllers/allergies');
 
 // GET /users
 router.get('/users/:id/allergies/new', isLoggedIn, allergiesCtrl.index)
-router.post('/allergies/new', isLoggedIn, allergiesCtrl.create);
+router.post('/users/<%= user._id %>/allergies/new', isLoggedIn, allergiesCtrl.create);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
