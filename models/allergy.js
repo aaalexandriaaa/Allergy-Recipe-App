49f3a2fce20937258a6dbb3e635c711e56fb7adc
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-01 18:41:16 -0400

    adding default values so they auto-populate

--- models/allergy.js
+++ models/allergy.js
@@ -2,14 +2,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var allergySchema = Schema({
-    gluten: Boolean,
-    dairy: Boolean,
-    eggs: Boolean,
-    finFish: Boolean,
-    shellfish: Boolean,
-    treeNuts: Boolean,
-    peanuts: Boolean,
-    soybeans: Boolean
+    gluten: {
+        type: Boolean,
+        default: false
+    },
+    dairy: {
+        type: Boolean,
+        default: false
+    },
+    eggs: {
+        type: Boolean,
+        default: false
+    },
+    finFish: {
+        type: Boolean,
+        default: false
+    },
+    shellfish: {
+        type: Boolean,
+        default: false
+    },
+    treeNuts: {
+        type: Boolean,
+        default: false
+    },
+    peanuts: {
+        type: Boolean,
+        default: false
+    },
+    soybeans: {
+        type: Boolean,
+        default: false
+    }
 }, {
     timestamps: true
 });
