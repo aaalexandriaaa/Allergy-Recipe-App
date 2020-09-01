e7f3c76545f614dce2303367d71be5c12c4df3ec
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-08-31 20:14:02 -0400

    create and index allergy functions work

--- controllers/allergies.js
+++ controllers/allergies.js
@@ -1,43 +1,11 @@
 const Allergy = require('../models/allergy');
 const User = require('../models/user');
 const user = require('../models/user');
-const {
-    NotImplemented
-} = require('http-errors');
-
 
 module.exports = {
-    create,
-    index
+    index,
+    create
 };
-
-function create(req, res) {
-    User.findById(req.user._id)
-        .then((user) => {
-            // if (user.allergies) {
-            Allergy.create(req.body)
-                .then((allergy) => {
-                    console.log(req.body)
-                    console.log(user.name)
-                    console.log(allergy._id)
-                    user.allergies = allergy._id
-                    console.log(user.allergies)
-                    res.redirect('/users/profile')
-                })
-            // } else {
-            //     console.log("ELSE")
-            //     console.log(req.body)
-            //     console.log(user.allergies)
-            //     Allergy.findByIdAndUpdate(user.allergies, req.body, {
-            //             new: true
-            //         })
-            //         .then((allergy) => {
-            //             res.redirect('/users/profile')
-            //         })
-            // }
-        })
-}
-
 
 function index(req, res) {
     console.log("INDEX")
@@ -56,6 +24,36 @@ function index(req, res) {
                 })
         })
 }
+
+function create(req, res) {
+    Allergy.create(req.body)
+        .then((allergy) => {
+            console.log(allergy)
+            console.log(allergy._id)
+            User.findById(req.user._id)
+                .then((user) => {
+                    console.log(user)
+                    user.allergies = allergy._id
+                    res.render('./users/profile', {
+                        title: 'allergies',
+                        allergy,
+                        user
+                    })
+                })
+        })
+}
+// User.findById(req.user._id)
+//     .then((user) => {
+//         Allergy.create(req.body)
+//             .then((allergy) => {
+//                 console.log(req.body)
+//                 console.log(user.name)
+//                 console.log(allergy._id)
+//                 user.allergies = allergy._id
+//                 console.log(user.allergies)
+//                 res.redirect('/users/profile')
+//             })
+//     })
 
 
 // CREATING A NEW ALLERGY DB ENTRY: 
