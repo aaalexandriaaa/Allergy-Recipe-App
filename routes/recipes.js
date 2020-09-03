0297417c1bce70513d80b3c0d9556efc20d3da40
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-03 09:26:21 -0400

    search and update routes and beginning of isCreator route fn

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,10 +1,13 @@
 var router = require('express').Router();
 var recipesCtrl = require('../controllers/recipes');
+const recipe = require('../models/recipe');
 
 // GET /users
 router.get('/recipes/search', isLoggedIn, recipesCtrl.index);
 router.get('/recipes/new', isLoggedIn, recipesCtrl.new)
 router.post('/recipes/new', isLoggedIn, recipesCtrl.create)
+router.post('/recipes/search', isLoggedIn, recipesCtrl.search)
+router.put('/recipes/:id/update', isLoggedIn, recipesCtrl.update)
 
 
 //router.get('/users/profile', isLoggedIn, usersCtrl.showProfile);
@@ -13,6 +16,11 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect("/auth/google");
 }
+
+// function isCreator(req, res, next) {
+//     if (req.isAuthenticated() && req.user._id === recipe.user) return next();
+//     res.redirect("/auth/google");
+// }
 
 
 module.exports = router;
